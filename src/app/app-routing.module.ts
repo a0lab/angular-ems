6cd347a52ac726d8f2993e7560fa3112e02d2215
Jayaraman Pancharathinam <jayaram.mailbox@gmail.com>
2022-04-11 00:37:01 +0900

    Angular Home component added for reference.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeCreateComponent } from './employee/employee-create/employee-create.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { EmployeeUpdateComponent } from './employee/employee-update/employee-update.component';
+import { AngularHomeComponent } from './help/angular-home/angular-home.component';
 
 const routes: Routes = [
   { path: "employees", component: EmployeeListComponent },
   { path: "employee-create", component: EmployeeCreateComponent},
   { path: "employee-update/:id", component: EmployeeUpdateComponent},
+  { path: "help/angular-home", component: AngularHomeComponent},
   { path: "", redirectTo: "employees", pathMatch: "full" }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { EmployeeCreateComponent } from './employee/employee-create/employee-create.component';
 import { EmployeeUpdateComponent } from './employee/employee-update/employee-update.component';
+import { AngularHomeComponent } from './help/angular-home/angular-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
     EmployeeCreateComponent,
-    EmployeeUpdateComponent
+    EmployeeUpdateComponent,
+    AngularHomeComponent
   ],
   imports: [
     BrowserModule,
